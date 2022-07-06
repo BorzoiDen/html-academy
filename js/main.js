d3f16a64b8e5938ade1d8e737d392e936e06f456
BorzoiDen <borozenetsdn@gmail.com>
2022-07-06 18:46:01 +0700

    added miniartures.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,7 @@
 //Imports
-import { photos } from './data.js';
+import  photos  from './data.js';
+import renderPhotos  from './miniatures.js';
 
-//HW 2-2-2.10
-//My code
-
-//const checkMaxLength = (string, maxLength)=> {
-//  if (string.length > maxLength) {
-//    return false;
-//  }
-//  return true;
-//};
-//checkMaxLength('Ghbdtn rfr ltkf', 15);
-
-// Answer
-const checkMaxLength = (string, maxLength)=> {
-  return string.length <= maxLength;
-};
-checkMaxLength('Ghbdtn rfr ltkf', 15);
-
-
-//=======================================
-
-//HW 2-3-3.11
 
 photos;
+renderPhotos;
